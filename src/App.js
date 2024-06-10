fbfa809259ceff1b63598a92d7b5c021b6b04140
Kyle Marunda <97623896+Elyk-Adnuram@users.noreply.github.com>
2024-06-10 16:19:32 +0200

    background colour added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
 import "./App.css";
-import Nationality from "./components/Nationality";
-import Age from "./components/Age";
-import Gender from "./components/Gender";
-
+import CustomizedButtons from "./components/Button";
 import { useState, useRef, useEffect } from "react";
+import * as React from "react";
+import SimpleAlert from "./components/SimpleAlert";
 
 function App() {
   const [name, setName] = useState(""); // state created for user input
-
+  const [nationalityInfo, setNationalityInfo] = useState({
+    country_id: "",
+    probability: "",
+  });
+  const [ageInfo, setAgeInfo] = useState("");
+  const [genderInfo, setGenderInfo] = useState("");
   const inputRef = useRef(); //useRef initialized
   //useRef hook used to auto-focus on an input field
   useEffect(() => {
     inputRef.current.focus();
   }, []);
 
+  //regex to ensure user only enters letters before making API call
+  const isNameValid = (name) => {
+    const regex = /^[a-zA-Z\s]+$/;
+    return regex.test(name);
+  };
+
+  // Perform API calls to fetch data
+  const fetchData = async () => {
+    if (!name || !isNameValid(name) || name.trim().length === 0) {
+      <SimpleAlert />;
+      alert("Please enter a valid name");
+      return;
+    }
+    //used Promise.all to fetch all the data at once
+    try {
+      const [nationalityRes, ageRes, genderRes] = await Promise.all([
+        fetch(`https://api.nationalize.io?name=${encodeURIComponent(name)}`),
+        fetch(`https://api.agify.io?name=${encodeURIComponent(name)}`),
+        fetch(`https://api.genderize.io?name=${encodeURIComponent(name)}`),
+      ]);
+      //used destructuring to assign the data from the API responses to variables
+      const [nationalityData, ageData, genderData] = await Promise.all([
+        nationalityRes.json(),
+        ageRes.json(),
+        genderRes.json(),
+      ]);
+
+      setNationalityInfo(nationalityData.country[0]);
+      setAgeInfo(JSON.stringify(ageData.age));
+      setGenderInfo(JSON.stringify(genderData.gender));
+      setName("");
+    } catch (error) {
+      console.error("An error has occurred: ", error.message);
+    }
+  };
+
   return (
     <div className="App">
-      <h1>Check your predicted Nationality and Estimated Age</h1>
+      <h1 className="header">Predictable</h1>
+      <h3>Enter your name and we will predict your nationality, age and gender.</h3>
       <input
         ref={inputRef}
         placeholder="Please enter your name"
@@ -28,10 +69,19 @@ function App() {
       />
       <br />
       <br />
-      <br />
-      <Nationality name={name} />
-      <Age name={name} />
-      <Gender name={name} />
+
+      <CustomizedButtons handleClick={fetchData} buttonName="Go For It!" />
+      {nationalityInfo && ageInfo && genderInfo && (
+        <div className="output">
+          <p>
+            Based on the name entered, there is a{" "}
+            {Math.round(Number(nationalityInfo.probability) * 100)} percent probability that your
+            nationality is {JSON.stringify(nationalityInfo.country_id)}
+          </p>
+          <p>Your predicted gender is: {genderInfo}</p>
+          <p>Your estimated AGE is: {ageInfo}</p>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/SimpleAlert.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+import Alert from "@mui/material/Alert";
+// import CheckIcon from "@mui/icons-material/Check";
+
+export default function SimpleAlert() {
+  return <Alert severity="success">Please enter a valid name</Alert>;
+}
